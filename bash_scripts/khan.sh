5b58002e565ac0907aa6c899bacd67a58fa25f2e
donnate <cdonnat@stanford.edu>
2019-11-19 15:52:37 -0800

    Update khan.sh

--- bash_scripts/khan.sh
+++ bash_scripts/khan.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 #SBATCH --job-name=khan_submit$1_$2
-#SBATCH --output=/scratch/users/cdonnat/convex_clustering/HC_dev/experiments/logs/khan.out
-#SBATCH --error=/scratch/users/cdonnat/convex_clustering/HC_dev/experiments/logs/khan.err
+#SBATCH --output=/scratch/users/cdonnat/convex_clustering/HC_dev/experiments/logs/khan_$1_$2.out
+#SBATCH --error=/scratch/users/cdonnat/convex_clustering/HC_dev/experiments/logs/khan_$1_$2.err
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
@@ -21,5 +21,5 @@ ml py-scipy/1.1.0_py36
  
 # execute script
 cd $SCRATCH/convex_clustering/HC_dev/experiments
-FILENAME=khan_train$1_lap_$2
+FILENAME=khan_train$1_lap_$2.pkl
 python3 khan_experiment.py -t $1 -type_lap $2 -savefile ${FILENAME} 
