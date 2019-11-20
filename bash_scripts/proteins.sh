1a217189ec9374580a6b58d70272a3094de1147c
donnate <cdonnat@stanford.edu>
2019-11-19 16:30:52 -0800

    Update proteins.sh

--- bash_scripts/proteins.sh
+++ bash_scripts/proteins.sh
@@ -21,5 +21,5 @@ ml py-scipy/1.1.0_py36
  
 # execute script
 cd $SCRATCH/convex_clustering/HC_dev/experiments
-FILENAME=protein_lap_$1.pkl
+FILENAME=protein_$2_lap_$1.pkl
 python3 protein_experiment.py-a $2 -type_lap $1 -savefile ${FILENAME} 
